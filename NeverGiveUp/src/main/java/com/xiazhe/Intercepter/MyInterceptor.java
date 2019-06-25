3cb30b059c7a3c950766abdc83026c1732f0ce42
xiazhefengzhi <591465401@qq.com>
2019-06-25 20:47:34 +0800

    修改了

--- NeverGiveUp/src/main/java/com/xiazhe/Intercepter/MyInterceptor.java
+++ NeverGiveUp/src/main/java/com/xiazhe/Intercepter/MyInterceptor.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class MyInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("pre");
+
         return true;
     }
 
