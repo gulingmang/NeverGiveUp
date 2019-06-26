c57038ce06c5f18e4e138c0a6c26e38b34a2ddd1
manggo <MangGo@vip.163.com>
2019-06-26 23:46:42 +0800

    设备种类提交

---
+++ NeverGiveUp/src/main/java/com/xiazhe/mapper/DeviceMapper.java
@@ -0,0 +1,12 @@
+package com.xiazhe.mapper;
+
+import com.xiazhe.bean.DeviceType;
+
+import java.util.List;
+
+public interface DeviceMapper {
+
+    /*显示所有设备种类*/
+    List queryAllDeviceType();
+
+}
---
+++ NeverGiveUp/src/main/java/com/xiazhe/service/DeviceService.java
@@ -0,0 +1,10 @@
+package com.xiazhe.service;
+
+import com.xiazhe.bean.DeviceType;
+
+import java.util.List;
+
+public interface DeviceService {
+    /*显示所有设备种类*/
+    List queryAllDeviceType();
+}
