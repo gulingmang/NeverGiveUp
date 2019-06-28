469ac40ff2f9c8f93249705ca0996ce977771129
xiazhefengzhi <591465401@qq.com>
2019-06-28 10:19:10 +0800

    Delete DepartmentService.java

--- NeverGiveUp/src/main/java/com/xiazhe/service/departmentService/DepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.xiazhe.service.DepartmentService;
-
-import com.xiazhe.bean.Department;
-
-import java.util.List;
-
-public interface DepartMentService {
-    List<Department> getList();
-}
