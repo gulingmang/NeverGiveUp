4471109be3c12eac24021cc8da291f3a2e0bfb5e
zhengxiang <308052313@qq.com>
2019-06-28 23:46:56 +0800

    完成了设备维修两种模糊查询

--- NeverGiveUp/src/test/java/MyTest1/ThisisMytt.java
+++ NeverGiveUp/src/test/java/MyTest1/ThisisMytt.java
@@ -1,14 +1,10 @@
 package MyTest1;
 
 import com.github.pagehelper.PageHelper;
-<<<<<<< HEAD
-import com.xiazhe.bean.Department;
-import com.xiazhe.bean.Employee;
-import com.xiazhe.bean.Order;
-import com.xiazhe.bean.UnQualifyApply;
-=======
+
+
 import com.xiazhe.bean.*;
->>>>>>> bfae0866c1b2c952b64955d3decd00399fd89ab6
+
 import com.xiazhe.config.ApplicationIniter;
 import com.xiazhe.config.SpringConfig;
 import com.xiazhe.config.SpringMvcConfig;
@@ -16,13 +12,11 @@ import com.xiazhe.controller.unqualifyManger.unqualifyController;
 import com.xiazhe.mapper.EmployeeMapper;
 import com.xiazhe.service.DepartmentService.DepartmentServiceImpl;
 import com.xiazhe.service.EmployeeService.EmployeeService;
+
 import com.xiazhe.service.finalMeasureCheckService.FinalMeasuretService;
-<<<<<<< HEAD
+
 import com.xiazhe.service.plan.OrderServicesImpl;
-=======
-import com.xiazhe.service.technologyService.TechnologyPlanService;
-import com.xiazhe.service.technologyService.TechnologyRequirementService;
->>>>>>> bfae0866c1b2c952b64955d3decd00399fd89ab6
+
 import com.xiazhe.service.unqulifyControllerService.UnqualifyServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -44,10 +38,7 @@ public class ThisisMytt {
     DepartmentServiceImpl my1;
     @Autowired
     EmployeeService service;
-    @Autowired
-    TechnologyRequirementService myservice;
-    @Autowired
-    TechnologyPlanService technologyPlanService;
+
     @Test
     public void Tets() {
         PageHelper pageHelper = new PageHelper();
@@ -67,13 +58,7 @@ public class ThisisMytt {
         System.out.println(list);
     }
 
-    @Test
-    public void Tets1333() {
-        ArrayList<TechnologyPlan> technologyPlans = technologyPlanService.queryAllTechnology();
 
-        System.out.println(technologyPlans);
-
-    }
     @Test
     public  void yyyy(){
         String[] s={"5"};
