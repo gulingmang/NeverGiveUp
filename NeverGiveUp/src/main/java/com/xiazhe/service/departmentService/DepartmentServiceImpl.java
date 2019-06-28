a9c403bd0c5d43c1f165fcdc974262ddd2409ab3
zhengxiang <308052313@qq.com>
2019-06-28 19:46:49 +0800

    ..

--- NeverGiveUp/src/main/java/com/xiazhe/service/departmentService/DepartmentServiceImpl.java
+++ NeverGiveUp/src/main/java/com/xiazhe/service/departmentService/DepartmentServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class DepartmentServiceImpl implements DepartMentService {
+public class DepartmentServiceImpl implements DepartmentService {
     @Autowired
     DepartmentMapper departmentMapper;
 
