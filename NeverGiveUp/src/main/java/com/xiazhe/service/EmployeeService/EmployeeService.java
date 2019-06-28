ccd17573af8ae97f80fe6be59073eb4e194db358
Nael <zx6932@outlook.com>
2019-06-28 19:03:22 +0800

    完成员工模块的功能

--- NeverGiveUp/src/main/java/com/xiazhe/service/EmployeeService/EmployeeService.java
+++ NeverGiveUp/src/main/java/com/xiazhe/service/EmployeeService/EmployeeService.java
@@ -1,9 +1,25 @@
 package com.xiazhe.service.EmployeeService;
 
 import com.xiazhe.bean.Employee;
+import com.xiazhe.bean.EmployeeDepartmentUnion;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface EmployeeService {
     List<Employee> getList();
+    /*插入数据*/
+    int insert(Employee record);
+    /*更新数据*/
+    int updateByPrimaryKey(Employee record);
+    /*所有员工及其部门的列表*/
+    List<EmployeeDepartmentUnion> queryAllEmployeesAndDepartment();
+    /*根据员工id进行模糊查询*/
+    List<EmployeeDepartmentUnion> queryEmployeesAndDepartmentById(@Param("empId") String empId);
+    /*根据员工姓名进行模糊查询*/
+    List<EmployeeDepartmentUnion> queryEmployeesAndDepartmentByEmpName(@Param("empName") String empName);
+    /*根据部门名称进行模糊查询*/
+    List<EmployeeDepartmentUnion> queryEmployeesAndDepartmentByDepName(@Param("depName") String depName);
+    /*根据员工id批量删除员工*/
+    int deleteEmployeesByIds(@Param("ids")String[] ids);
 }
--- NeverGiveUp/src/main/java/com/xiazhe/service/EmployeeService/EmployeeServiceImpl.java
+++ NeverGiveUp/src/main/java/com/xiazhe/service/EmployeeService/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.xiazhe.service.EmployeeService;
 
 import com.xiazhe.bean.Employee;
+import com.xiazhe.bean.EmployeeDepartmentUnion;
 import com.xiazhe.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,40 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> getList() {
         List<Employee> list = mymapper.getList();
         return list;
+    }
+
+    @Override
+    public int insert(Employee record) {
+        return mymapper.insert(record);
+    }
+
+    @Override
+    public int updateByPrimaryKey(Employee record) {
+        return mymapper.updateByPrimaryKey(record);
+    }
+
+    @Override
+    public List<EmployeeDepartmentUnion> queryAllEmployeesAndDepartment() {
+        return mymapper.queryAllEmployeesAndDepartment();
+    }
+
+    @Override
+    public List<EmployeeDepartmentUnion> queryEmployeesAndDepartmentById(String empId) {
+        return mymapper.queryEmployeesAndDepartmentById(empId);
+    }
+
+    @Override
+    public List<EmployeeDepartmentUnion> queryEmployeesAndDepartmentByEmpName(String empName) {
+        return mymapper.queryEmployeesAndDepartmentByEmpName(empName);
+    }
+
+    @Override
+    public List<EmployeeDepartmentUnion> queryEmployeesAndDepartmentByDepName(String depName) {
+        return mymapper.queryEmployeesAndDepartmentByDepName(depName);
+    }
+
+    @Override
+    public int deleteEmployeesByIds(String[] ids) {
+        return mymapper.deleteEmployeesByIds(ids);
     }
 }
